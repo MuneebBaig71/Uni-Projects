999602d5e5526b0cf136cd2e6bd4c54ad9e05a48
Muneeb Baig <59503785+MuneebBaig71@users.noreply.github.com>
2022-05-11 10:12:14 -0700

    Add files via upload

---
+++ JS uni/6.17.js
@@ -0,0 +1,15 @@
+
+var a = window.prompt("Enter a number");
+var b = window.prompt("Enter a number");
+if(a<b)
+{
+    alert("a is  smaller");
+}
+else if(a>b)
+{
+    alert("b is smaller");
+}
+else(a==b)
+{
+    document.write("<h1>a and b are equal </h1>");
+}
---
+++ JS uni/6.5.js
@@ -0,0 +1,40 @@
+//b 
+var i ,j ,k ,sum;
+
+//c
+var iVal;
+var jVal;
+var kVal;
+
+//d
+var a = window.prompt("Enter first value" , "0");
+iVal = parseInt(a);
+console.log("iVal is " + iVal);
+
+
+//e
+var b = window.prompt("Enter first value" , "5");
+jVal = parseInt(b);
+console.log("jVal is " + jVal);
+
+
+//d
+var c = window.prompt("Enter first value" , "10");
+kVal = parseInt(c);
+console.log("kVal is " + kVal);
+
+
+//e
+var i = parseInt(iVal);
+
+//f
+var j = parseInt(jVal);
+
+//g
+var k = parseInt(kVal);
+
+//h
+sum = i + j + k;
+
+//i
+document.writeln("<h1>Sum of is " + sum +"</h1>");
